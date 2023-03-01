78925c86db02baa02d65248737b23101533c30c0
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-03-01 10:42:04 -0500

    successfully loaded data from real estate api to S3

--- app.py
+++ app.py
@@ -9,26 +9,6 @@ from datetime import date
 load_dotenv()
 
 
-# def load_to_s3(client, data):
-# 	client = boto3.client(
-# 	    's3', 
-# 	    endpoint_url=os.getenv('ENDPOINT_URL'),
-# 	    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-# 	    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
-# 	)
-# 	json_data = json.dumps(data)
-# 	client.put_object(
-# 		Bucket=os.getenv('BUCKET_NAME'), 
-# 		Key=f'real_estate/listings/{date.today()}.json',
-# 		Body=json_data
-# 	)
-# 	client.put_object(
-# 	    Bucket=os.getenv('BUCKET_NAME'), 
-# 	    Key=f'real_estate/listings/latest.json',
-# 	    Body=json_data
-# 	)
-
-
 def get_list_of_zip_codes():
 	conn_params = {
 		'user': os.getenv('SNOWFLAKE_USERNAME'),
@@ -47,24 +27,39 @@ def get_list_of_zip_codes():
 
 def get_results_for_zipcodes(zipcodes):
 	url = 'https://realty-mole-property-api.p.rapidapi.com/saleListings'
-	querystring = {'state':'DE', 'limit': '500', 'propertyType': 'Single Family', 'status': 'Active'}
+	querystring = {'state':'DE', 'limit': '500', 'propertyType': 'Single Family'}
 	headers = {
 		'X-RapidAPI-Key': os.getenv('RAPID_API_KEY'),
 		'X-RapidAPI-Host': 'realty-mole-property-api.p.rapidapi.com'
 	}
-	count = 0
+	results = []
 	for zip in zipcodes:
 		sleep(.6)
 		querystring['zipCode'] = zip
 		response = requests.get(url=url, headers=headers, params=querystring)
-		data = response.json()
-		with open(f'raw_data/result_{count}.json', 'w') as f:
-			json.dump(data, f)
-		count += 1
+		results.append(response.json())
+	return results
+
+def load_results_to_s3(results):
+	client = boto3.client(
+	    's3', 
+	    endpoint_url='https://s3.amazonaws.com',
+	    aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
+	    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
+	)
+	for i, obj in enumerate(results):
+		if len(obj) > 0:
+			json_data = json.dumps(obj)
+			client.put_object(
+				Bucket=os.getenv('BUCKET_NAME'), 
+				Key=f'real_estate/listings/{date.today()}/result_{i}.json',
+				Body=json_data
+			)
 
 def main():
 	zipcodes = get_list_of_zip_codes()
-	get_results_for_zipcodes(zipcodes)
+	results = get_results_for_zipcodes(zipcodes)
+	load_results_to_s3(results)
 
 
 if __name__ == '__main__':
